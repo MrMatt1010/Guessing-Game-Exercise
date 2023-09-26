a45acda8648474993a83efef11aa48e11f6842cd
Matt T <mrsymtec@gmail.com>
2023-09-26 14:39:41 +1300

    Added comments to the code and made sure it displayed properly in the terminal.

--- Guessing-Game/Guessing-Game.cpp
+++ Guessing-Game/Guessing-Game.cpp
@@ -4,17 +4,20 @@
 
 
 int main() {
-
+	// This code is just to add the title of the game and instructions
 	std::cout << "\t\t\tGuess Game\n";
 	std::cout << "\t\t\t**********\n";
 
 	std::cout << "You will get three chances to guess the number\n ";
 	std::cout << "Guess the number between one and ten\n ";
 
+
+	// This part of the program generates the random number
 	srand(time(0));
 	int randomNumber = (rand() % (10 - 1)) + 1;
 	int guess;
 
+	//This is where the players guesses are made and the correct output is given
 	int chancesLeft = 3;
 	for (int i = 1; i <= 3; i++) {
 		std::cout << "\n\nEnter your guess ";
@@ -24,11 +27,11 @@ int main() {
 			break;
 		}
 		else if (guess > randomNumber) {
-			std::cout << guess << " Sorry wrong guess, try again";
+			std::cout << guess << " Was the wrong guess, try again ";
 		}
 		chancesLeft--;
 		if (chancesLeft == 0) {
-			std::cout << "\nBetter luck next time";
+			std::cout << "\nBetter luck next time ";
 			std::cout << randomNumber << " Was the correct number";
 		}
 	}
